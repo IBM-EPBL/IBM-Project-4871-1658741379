bfbcc4007b4e11f191e5bebf01668451bd4a56c7
Inbanila <113909244+Inbanila@users.noreply.github.com>
2022-11-25 00:08:37 -0800

    Add files via upload

---
+++ Maryj.java
@@ -0,0 +1,20 @@
+import java.util.scanner;
+class Maryj
+{
+public static void main(String [] args)
+{
+int a;
+Scanner sc=new Scanner(System.in);
+System.out.println("enter the number");
+a=sc.nextInt();
+for(int i=0;i<=a;i++)
+{
+System.out.println(i);
+}
+}
+}
+
+
+
+
+
